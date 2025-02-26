85d659363625631b43f86bf0d601ec7fd15ed3ce
Anoshkin Vitalii <Anoshkin.home@gmail.com>
2025-02-26 17:10:27 +0200

    Implemented the PopularItem.

--- lib/presentation/home_page/home_page.dart
+++ lib/presentation/home_page/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
+import 'package:ingadb/domain/cubit/game/game_cubit.dart';
 import 'package:ingadb/presentation/home_page/item/ad.dart';
 import 'package:ingadb/presentation/home_page/item/discover_full_catalog.dart';
 import 'package:ingadb/presentation/home_page/item/for_you.dart';
@@ -11,32 +14,35 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Row(
-          children: [
-            Icon(Icons.home),
-            SizedBox(width: 10),
-            Text(
-              'Home',
-              style: TextStyle(
-                fontSize: 24,
-                letterSpacing: 5.0,
-                fontWeight: FontWeight.w500,
+    return BlocProvider<GameCubit>(
+      create: (_) => GetIt.I.get()..fetchGames(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Row(
+            children: [
+              Icon(Icons.home),
+              SizedBox(width: 10),
+              Text(
+                'Home',
+                style: TextStyle(
+                  fontSize: 24,
+                  letterSpacing: 5.0,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
-            ),
+            ],
+          ),
+        ),
+        body: const CustomScrollView(
+          slivers: <Widget>[
+            PopularItem(),
+            DiscoverFullCatalog(),
+            RecentReviews(),
+            AdItem(),
+            ForYou(),
+            GameGenresCatalog(),
           ],
         ),
-      ),
-      body: const CustomScrollView(
-        slivers: <Widget>[
-          PopularItem(),
-          DiscoverFullCatalog(),
-          RecentReviews(),
-          AdItem(),
-          ForYou(),
-          GameGenresCatalog(),
-        ],
       ),
     );
   }
--- lib/presentation/home_page/item/popular.dart
+++ lib/presentation/home_page/item/popular.dart
@@ -1,11 +1,14 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:ingadb/config/theme/colors.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ingadb/domain/cubit/game/game_cubit.dart';
 
 class PopularItem extends StatelessWidget {
   const PopularItem({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final games = context.select((GameCubit cubit) => cubit.state.games);
     return SliverToBoxAdapter(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -24,24 +27,26 @@ class PopularItem extends StatelessWidget {
             height: 200,
             child: ListView.builder(
               scrollDirection: Axis.horizontal,
-              itemCount: 20,
+              itemCount: games.length,
               itemBuilder: (context, index) {
+                final game = games[index];
                 return Padding(
                   padding: const EdgeInsets.all(4.0),
                   child: ClipRRect(
-                    borderRadius: BorderRadius.circular(12.0),
-                    child: Container(
+                    borderRadius: BorderRadius.circular(12),
+                    child: CachedNetworkImage(
+                      memCacheWidth: MediaQuery.of(context).size.width.toInt(),
+                      imageUrl: game.backgroundImage,
                       width: 150,
-                      color: AppColors.lightGrey,
-                      child: Center(
-                        child: Text(
-                          'Item $index',
-                          style: const TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
+                      progressIndicatorBuilder:
+                          (context, url, downloadProgress) => Center(
+                        child: CircularProgressIndicator(
+                          value: downloadProgress.progress,
                         ),
                       ),
+                      errorWidget: (context, url, error) =>
+                          const Icon(Icons.error),
+                      fit: BoxFit.cover,
                     ),
                   ),
                 );
